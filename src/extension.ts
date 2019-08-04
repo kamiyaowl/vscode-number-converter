d221745c0c9b758fa45262c70ff55b1efcb6d4e0
K <kamiyaowl@gmail.com>
2019-08-04 17:02:43 +0900

    icon追加

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,13 @@
 import * as vscode from 'vscode';
 import * as parse from './parse';
 
+function showResult(src: string) {
+	const dst = parse.parse(src as string);
+	dst.forEach(d => {
+		vscode.window.showInformationMessage(`${d.origin} | ${d.hex} | ${d.bin}`);
+	});
+	// console.log(dst);
+}
 export function activate(context: vscode.ExtensionContext) {
 	const commands = [
 		vscode.commands.registerCommand('extension.convert-from-input', () => {
@@ -8,25 +15,30 @@ export function activate(context: vscode.ExtensionContext) {
 				placeHolder: "e.g. 256, 0xff00abcd, 0b10101100...",
 			}).then(data => {
 				if (typeof data === 'string') {
-					const dst = parse.parse(data as string);
-					dst.forEach(d => {
-						vscode.window.showInformationMessage(`${d.origin} | ${d.hex} | ${d.bin}`);
-					});
-					// console.log(dst);
+					showResult(data);
 				} else {
 					vscode.window.showErrorMessage(`invalid data: ${data}`);
 				}
 			});
-			vscode.window.showInformationMessage('Convert!');
 		}),
 		vscode.commands.registerCommand('extension.convert', () => {
-			vscode.window.showInformationMessage('Convert!');
-		}),
-		vscode.commands.registerCommand('extension.increment', () => {
-			vscode.window.showInformationMessage('Increment!');
-		}),
-		vscode.commands.registerCommand('extension.decrement', () => {
-			vscode.window.showInformationMessage('Decrement!');
+			const editor = vscode.window.activeTextEditor;
+			if(editor) {
+				const selections = 
+					editor.selections
+						  .map(s => editor.document.getText(new vscode.Range(s.start, s.end)))
+						  .reverse() // 上から処理しているのにNotifyが逆順になるため
+						  .join(' ');
+				if (selections) {
+					// 選択範囲がある場合はそちらを優先
+					showResult(selections);
+				} else {
+					// なければ現在の行
+					const pos = editor.selection.active;
+					const textLine = editor.document.lineAt(pos.line);
+					showResult(textLine.text);
+				}
+			}
 		}),
 	];
 
